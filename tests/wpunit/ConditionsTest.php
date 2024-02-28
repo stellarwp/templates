4683dd23225995952c33864d0506e4a2216636a5
Tanner Record <tanner.record@gmail.com>
2024-02-28 16:09:08 -0500

    Add coding standards to fix inconsistencies

--- tests/wpunit/ConditionsTest.php
+++ tests/wpunit/ConditionsTest.php
@@ -1,46 +1,41 @@
-<?php
+<?php declare(strict_types=1);
 
 use StellarWP\Templates\Tests\TemplateTestCase;
 use StellarWP\Templates\Utils\Conditions;
 
 class ConditionsTest extends TemplateTestCase {
-    /**
+	/**
 	 * @test
 	 */
-    public function it_returns_true_with_bool()
-    {
+	public function it_returns_true_with_bool(): void {
 		$this->assertTrue( Conditions::is_truthy( true ) );
-    }
+	}
 
 	/**
 	 * @test
 	 */
-    public function it_returns_true_with_valid_string()
-    {
+	public function it_returns_true_with_valid_string(): void {
 		$this->assertTrue( Conditions::is_truthy( 'true' ) );
-    }
+	}
 
 	/**
 	 * @test
 	 */
-    public function it_returns_false_with_invalid_string()
-    {
+	public function it_returns_false_with_invalid_string(): void {
 		$this->assertFalse( Conditions::is_truthy( 'false' ) );
-    }
+	}
 
 	/**
 	 * @test
 	 */
-    public function it_returns_true_with_truthy_int()
-    {
+	public function it_returns_true_with_truthy_int(): void {
 		$this->assertTrue( Conditions::is_truthy( 1 ) );
-    }
+	}
 
 	/**
 	 * @test
 	 */
-    public function it_returns_false_with_falsy_int()
-    {
+	public function it_returns_false_with_falsy_int(): void {
 		$this->assertFalse( Conditions::is_truthy( 0 ) );
-    }
+	}
 }
--- tests/wpunit/PathsTest.php
+++ tests/wpunit/PathsTest.php
@@ -1,84 +1,84 @@
-<?php
+<?php declare(strict_types=1);
 
 use StellarWP\Templates\Tests\TemplateTestCase;
 use StellarWP\Templates\Utils\Paths;
 
 class PathsTest extends TemplateTestCase {
 
-    /**
+	/**
 	 * @test
 	 */
-    public function it_should_merge_two_strings_without_intersections() {
+	public function it_should_merge_two_strings_without_intersections(): void {
 		$expected = '/app/public/plugin/templates/src/views/product/list.php';
-		$actual = Paths::merge(
+		$actual   = Paths::merge(
 			'/app/public/plugin/templates/src/views',
 			'/product/list.php'
 		);
 
 		$this->assertSame( $expected, $actual );
-    }
+	}
 
 	/**
 	 * @test
 	 */
-    public function it_should_merge_two_strings_with_intersections() {
+	public function it_should_merge_two_strings_with_intersections(): void {
 		$expected = '/app/public/plugin/templates/src/views/product/list.php';
-		$actual = Paths::merge(
+		$actual   = Paths::merge(
 			'/app/public/plugin/templates/src/views',
 			'src/views/product/list.php'
 		);
 
 		$this->assertSame( $expected, $actual );
-    }
+	}
 
 	/**
 	 * @test
 	 */
-    public function it_should_merge_three_strings_with_intersections() {
+	public function it_should_merge_three_strings_with_intersections(): void {
 		$expected = '/app/public/plugin/templates/src/views/product/list.php';
-		$actual = Paths::merge(
+		$actual   = Paths::merge(
 			'/app/public/plugin/templates',
 			'/templates/src/views',
 			'src/views/product/list.php'
 		);
 
 		$this->assertSame( $expected, $actual );
-    }
+	}
 
 	/**
 	 * @test
 	 */
-    public function it_should_merge_three_strings_without_intersections() {
+	public function it_should_merge_three_strings_without_intersections(): void {
 		$expected = '/app/public/plugin/templates/src/views/product/list.php';
-		$actual = Paths::merge(
+		$actual   = Paths::merge(
 			'/app/public/plugin/templates',
 			'/src/views',
 			'/product/list.php'
 		);
 
 		$this->assertSame( $expected, $actual );
-    }
+	}
 
 	/**
 	 * @test
 	 */
-    public function it_should_merge_strings_with_escape_sequences() {
+	public function it_should_merge_strings_with_escape_sequences(): void {
 		$expected = '/app/public/my\ plugin/templates/src/views/product/list.php';
-		$actual = Paths::merge(
+		$actual   = Paths::merge(
 			'/app/public/my\ plugin/templates',
 			'/src/views',
 			'/product/list.php'
 		);
 
 		$this->assertSame( $expected, $actual );
-    }
+	}
 
 	/**
 	 * @test
 	 */
-    public function it_should_merge_path_fragments_without_intersections() {
+	public function it_should_merge_path_fragments_without_intersections(): void {
 		$expected = '/app/public/plugin/templates/src/views/product/list.php';
-		$actual = Paths::merge(
+		$actual   = Paths::merge(
 			[
 				'',
 				'app',
@@ -90,19 +90,19 @@ class PathsTest extends TemplateTestCase {
 				'src',
 				'views',
 				'product',
-				'list.php'
+				'list.php',
 			]
 		);
 
 		$this->assertSame( $expected, $actual );
-    }
+	}
 
 	/**
 	 * @test
 	 */
-    public function it_should_merge_path_fragments_with_intersections() {
+	public function it_should_merge_path_fragments_with_intersections(): void {
 		$expected = '/app/public/plugin/templates/src/views/product/list.php';
-		$actual = Paths::merge(
+		$actual   = Paths::merge(
 			[
 				'',
 				'app',
@@ -118,10 +118,10 @@ class PathsTest extends TemplateTestCase {
 				'src',
 				'views',
 				'product',
-				'list.php'
+				'list.php',
 			]
 		);
 
 		$this->assertSame( $expected, $actual );
-    }
+	}
 }
